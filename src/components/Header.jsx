dd24c59837145a4334628afb86c6f0d9416d6731
migsilva89 <migdrum@gmail.com>
2022-11-28 17:07:26 +0000

    fixx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
   return (
     <header className="text-center mb-16 mx-6 ">
       <div className='flex justify-center'>
-        <img src="/public/Dog-icon.png" alt="" />
+        <img src="/Dog-icon.png" alt="" />
       </div>
       <h1 className="text-4xl text-blue-600">Dogs Facts App</h1>
       <h2 className="text-blue-300">Just an app with facts of dogs.</h2>
