bee668f8065ebd9ecf9a0b50748ebeda2f827d91
migsilva89 <migdrum@gmail.com>
2022-11-28 17:05:44 +0000

    add img

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 function Header() {
   return (
-    <header className="text-center mb-16 mx-8 ">
+    <header className="text-center mb-16 mx-6 ">
+      <div className='flex justify-center'>
+        <img src="/public/Dog-icon.png" alt="" />
+      </div>
       <h1 className="text-4xl text-blue-600">Dogs Facts App</h1>
       <h2 className="text-blue-300">Just an app with facts of dogs.</h2>
     </header>
